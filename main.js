b853958cc1d5c43556bc9cc13a433d6e595f7917
GuilhermedeOliveiraOFC <o.guilherme02@escola.pr.gov.br>
2025-04-15 16:34:23 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -38,11 +38,7 @@ function register() {
   saveUsers(users);
 
   alert("Cadastro realizado com sucesso!");
-  document.getElementById("registerName").value = "";
-  document.getElementById("registerEmail").value = "";
-  document.getElementById("registerPassword").value = "";
-
-  listarUsuarios();
+  window.location.href = "login.html";
 }
 
 function login() {
@@ -59,7 +55,7 @@ function login() {
 
   if (user) {
     localStorage.setItem("usuarioLogado", JSON.stringify(user));
-    mostrarUsuarioLogado();
+    window.location.href = "bemvindo.html";
   } else {
     alert("Email ou senha inválidos!");
   }
@@ -79,50 +75,11 @@ function mostrarUsuarioLogado() {
 
 function logout() {
   localStorage.removeItem("usuarioLogado");
-  document.getElementById("usuarioLogadoContainer").innerHTML = "";
-}
-
-function downloadUsers() {
-  const users = getUsers();
-  const blob = new Blob([JSON.stringify(users, null, 2)], { type: "application/json" });
-  const url = URL.createObjectURL(blob);
-
-  const a = document.createElement("a");
-  a.href = url;
-  a.download = "users.json";
-  a.click();
-
-  URL.revokeObjectURL(url);
-}
-
-function listarUsuarios() {
-  const users = getUsers();
-  const listaContainer = document.getElementById("listaUsuarios");
-  listaContainer.innerHTML = "<h2>Usuários Cadastrados</h2>";
-
-  if (users.length === 0) {
-    listaContainer.innerHTML += "<p>Nenhum usuário cadastrado.</p>";
-    return;
-  }
-
-  users.forEach((user, index) => {
-    const div = document.createElement("div");
-    div.innerHTML = `
-      <p><strong>${user.name}</strong> - ${user.email}
-      <button onclick="removerUsuario(${index})">Remover</button></p>
-    `;
-    listaContainer.appendChild(div);
-  });
-}
-
-function removerUsuario(index) {
-  let users = getUsers();
-  users.splice(index, 1);
-  saveUsers(users);
-  listarUsuarios();
+  window.location.href = "login.html";
 }
 
 window.onload = function () {
-  mostrarUsuarioLogado();
-  listarUsuarios();
+  if (document.getElementById("usuarioLogadoContainer")) {
+    mostrarUsuarioLogado();
+  }
 };
